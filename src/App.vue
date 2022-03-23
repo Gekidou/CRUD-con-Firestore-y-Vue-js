ba5a4983ce826ed108a093ff2f15aad0a49caaed
Fabian <therealfabi@hotmail.com>
2022-03-23 17:53:57 -0300

    subiendo desafio

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Form></Form>
+    <Table></Table>
+    
+  </div>
+</template>
+
+<script>
+import Table from './components/Table.vue'
+import Form from './components/Form.vue'
+
+export default {
+  components: {
+    Table, Form
+  }
+}
+</script>
+
+<style>
+body{
+  font-family: 'Montserrat', sans-serif;
+}
+
+.container {
+  padding: 20px;
+}
+</style>
